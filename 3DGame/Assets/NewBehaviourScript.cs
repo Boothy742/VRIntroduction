38a1d45608021500c24283f21ec7679515b7fac5
Drew Morgan <dangerworm@gmail.com>
2019-04-07 18:30:43 +0100

    First commit. Made a capsule gun with Drew

---
+++ 3DGame/Assets/NewBehaviourScript.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public Transform Capsule;
+    public Transform Camera;
+
+    public int AmountOfForce;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            var position = Camera.position;
+            position.z += 5;
+
+            var newcapsule = Instantiate(Capsule, position, Quaternion.identity);
+            var newcapsuleBody = newcapsule.GetComponent<Rigidbody>();
+            newcapsuleBody.AddForce(Camera.forward * AmountOfForce, ForceMode.Force);
+        }
+    }
+}
